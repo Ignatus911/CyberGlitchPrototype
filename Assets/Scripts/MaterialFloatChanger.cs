64e43d099e2e852a763fce270b4137048e94cd75
Ignat <ignatnee@gmail.com>
2020-12-30 19:04:21 +0300

    Pull whole project
    Inital commit.

---
+++ Assets/Scripts/MaterialFloatChanger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class MaterialFloatChanger : MonoBehaviour
+{
+    [SerializeField] private Material material;
+    [SerializeField] private string floatName;
+    [SerializeField] private float floatValue;
+
+    private void Update()
+    {
+        material.SetFloat(floatName, floatValue);
+    }
+}
+
+
